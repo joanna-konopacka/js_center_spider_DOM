4304c7d4726a9e895d3c7c690901e13524dc079a
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-11 20:18:43 +0100

    solution-test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
 
 const centerX = (wall.clientWidth - spider.clientWidth) / 2;
 const centerY = (wall.clientHeight - spider.clientHeight) / 2;
 
-spider.style.position = 'absolute';
+spider.style.position = "absolute";
 spider.style.top = `${centerY}px`;
 spider.style.left = `${centerX}px`;
