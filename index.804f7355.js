bf26c6a21147391cbd460e9b4cec4c2e0798ffc7
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-12 01:04:12 +0100

    make build

---
+++ index.804f7355.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),l=(t.clientWidth-e.clientWidth)/2,c=(t.clientHeight-e.clientHeight)/2;e.style.position="absolute",e.style.top="".concat(c,"px"),e.style.left="".concat(l,"px");
+//# sourceMappingURL=index.804f7355.js.map
